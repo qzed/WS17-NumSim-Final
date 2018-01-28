8412a3b377839ec1ed283c0e96a76e3c5a5314da
qzed <qzed@users.noreply.github.com>
2018-01-28 21:34:46 +0100

    Add basic OpenCL header

---
+++ src/opencl/opencl.hpp
@@ -0,0 +1,11 @@
+//! Header for the (C++) OpenCL API.
+//!
+//! Include this instead of <CL/...>.
+
+#pragma once
+
+#define CL_HPP_ENABLE_EXCEPTIONS 1
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+
+#include <CL/cl2.hpp>
